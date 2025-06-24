888a4d493a035ec2595354679f2ba1fdd507ff76
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-06-24 11:33:19 +0200

    About to compress for findAllImports.

--- library/_commons/utilities/find-all-imports.js
+++ library/_commons/utilities/find-all-imports.js
@@ -9,7 +9,7 @@ import { getSourceCodeFromFilePath } from "get-sourcecode-from-file-path";
 // At that time, findAllImports will still take importPath as its first argument, but everything else that is currently optional will need to be inside an object...? Or what if I were to just do this now? And to do the same with all of my recursive, pre-parameterized arguments?
 
 /**
- * Processes recursively and resolves a single import path.
+ * Processes recursively and resolves a single import path. // (Unlike findAllImports, here `currentDir`, `cwd`, `visitedSet`, `depth`, and `maxDepth` aren't options because they are mandatory and not pre-parameterized.)
  * @param {string} importPath The import path currently being addressed.
  * @param {string} currentDir The directory containing the import path currently being addressed.
  * @param {string} cwd The current working directory, set as `process.cwd()` by default.
@@ -46,15 +46,21 @@ const processImport = (
  * @param {Set<string>} visitedSet The set of strings tracking the import paths that have already been visited, instantiated as a `new Set()` by default.
  * @param {number} depth The current depth of the recursion, instantiated at `0` by default.
  * @param {number} maxDepth The maximum depth allowed for the recursion, instantiated at `100` by default.
+ * @param {{cwd: string; visitedSet: Set<string>; depth: number; maxDepth: number}} options
  * @returns The complete set of strings of import paths recursively related to the given file path, or `null` if an issue has arisen.
  */
 export const findAllImports = (
   filePath,
-  cwd = process.cwd(),
-  visitedSet = new Set(),
-  depth = 0,
-  maxDepth = 100
+  options = {
+    cwd: process.cwd(),
+    visitedSet: new Set(),
+    depth: 0,
+    maxDepth: 100,
+  }
 ) => {
+  // Obtained the pre-paramaterized arguments from the options.
+  const { cwd, visitedSet, depth, maxDepth } = options;
+
   // Fails early if max depth is recursively reached.
   if (depth > maxDepth) {
     console.error(`ERROR. Max depth ${maxDepth} reached at ${filePath}.`);
