2ee5bff7be558810e62305c5331d708506e6d21a
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-07-29 22:11:50 +0200

    About to use comments.template.js.

--- comments.example.js
+++ comments.example.js
@@ -1,1 +1,7 @@
 // A JavaScript file that acts as an example file for the generated Comment Variables template config. Use the `comment-variables compress` and `comment-variables resolve` commands to see how the following comments go back and forth from Comment Variables placeholders to actual comments reversibly.
+
+// This is a comment.
+// $COMMENT#ALIAS
+// Yes.
+// This is a comment. Yes. This is a comment.
+// $COMMENT#COMMENTS#NO
--- comments.template.js
+++ comments.template.js
@@ -1,1 +1,25 @@
 // A Comment Variables config template generated upon approval in case no config file has been found. Feel free to use it as a steping stone to learn how to use Comment Variables.
+
+const data = Object.freeze({
+  comment: "This is a comment.",
+  alias: "COMMENT",
+  composed: "$COMMENT#COMMENT $COMMENT#COMMENTS#YES $COMMENT#ALIAS",
+  comments: Object.freeze({
+    yes: "Yes.",
+    no: "No.",
+  }),
+});
+
+const ignores = [];
+
+const lintConfigImports = false; // can be ommitted
+const myIgnoresOnly = false; // can be ommitted
+
+const config = {
+  data,
+  ignores,
+  lintConfigImports,
+  myIgnoresOnly,
+};
+
+export default config;
