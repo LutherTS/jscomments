c61ea13c4bb430660351ec364295057ff22dd806
Luther Tchofo Safo <nintendobserver@gmail.com>
2026-05-08 20:09:31 +0200

    Updated variations template too.

--- generate.variations.js
+++ generate.variations.js
@@ -1,11 +1,10 @@
 // The Comment Variables config template generated in case no config file has been found.
 
-// Just flip the `variations` object's `variant` key back and forth between `EN` and `FR` and see what happens on the accompanying `comments.example.js` file that has also been generated.
-
 /* variants */
 
 const EN = "en";
 const ENGLISH = "English";
+
 const FR = "fr";
 const FRANÇAIS = "français";
 
@@ -14,9 +13,9 @@ const FRANÇAIS = "français";
 const enData = Object.freeze({
   comment: "This is a comment.",
   alias: "EN#COMMENT",
-  composed: "$COMMENT#EN#COMMENT $COMMENT#EN#COMMENTS#YES $COMMENT#EN#ALIAS",
+  composed: "$COMMENT#EN#COMMENT $COMMENT#EN#COMMENTS#YES. $COMMENT#EN#ALIAS",
   comments: Object.freeze({
-    yes: "Yes.",
+    yes: "Yes",
     no: "No.",
   }),
 });
@@ -24,9 +23,9 @@ const enData = Object.freeze({
 const frData = Object.freeze({
   comment: "Ceci est un commentaire.",
   alias: "FR#COMMENT",
-  composed: "$COMMENT#FR#COMMENT $COMMENT#FR#COMMENTS#YES $COMMENT#FR#ALIAS",
+  composed: "$COMMENT#FR#COMMENT $COMMENT#FR#COMMENTS#YES. $COMMENT#FR#ALIAS",
   comments: Object.freeze({
-    yes: "Oui.",
+    yes: "Oui",
     no: "Non.",
   }),
 });
@@ -57,7 +56,7 @@ const frComposedVariablesExclusives = ["FR#COMMENTS#YES"];
 const composedVariablesExclusives = [
   ...enComposedVariablesExclusives,
   ...frComposedVariablesExclusives,
-]; // can be omitted, composed variables allowed
+]; // composed variables allowed, Comment Variables that include `#COMPOSEDVARIABLESEXCLUSIVES#` are implicitly added
 
 /* variations */
 
@@ -73,9 +72,19 @@ const variations = Object.freeze({
   referenceData: enData,
   // Defines the variant of the reference variation.
   referenceVariant: EN,
-  // Defines the behavior of the error handling in case of variations that do not match one-to-one with the reference variation. If `true`, allows incomplete variations data to remain. If `false`, errors and guides the fixing of missing variations data.
+  // Defines the behavior of the error handling in case of variations that do not match one-to-one with the reference variation. If `true`, allows incomplete variations data to remain. If `false`, errors and guides the fixing of missing variations data (while ignoring composed variables exclusives).
   allowIncompleteVariations: true,
-}); // can be omitted
+  // Defines which variants' `#PUBLIC#` Comment Variables should be publicly available, by default through `comments.public.mjs` and `comments.public.json`.
+  // public: [EN, FR] // v3
+});
+
+// /* libraries */
+
+// import { commentVariablesData as libraryCommentVariablesData } from "library"
+
+// const libraries = {
+//   "library": libraryCommentVariablesData.variantKey
+// } // v3
 
 const config = {
   data,
@@ -84,10 +93,17 @@ const config = {
   myIgnoresOnly,
   composedVariablesExclusives,
   variations,
+  // libraries // v3
 };
 
 export default config;
 
-// Once you've grasped these concepts, simply rename this file, its JSON counterpart and its `.mjs` counterpart to `comments.config.js`, `comments.config.json` and `comments.config.mjs` respectively and you're good to go. (If you're using the extension, make sure to run VS Code's "Developer: Reload Window" command for the extension to operate based on your new `comments.config.js` file.)
+// Just flip the `variations` object's `variant` key back and forth between `EN` and `FR` and see what happens when running commands and hovering on the accompanying `comments.example.js` file that has also been generated.
 
-// When it comes to the VS Code extension, variations are only compatible with versions 2 and above.
+// The `comment-variables resolve` command resolves Comment Variables into comments.
+// The `comment-variables compress` command compresses comments into Comment Variables.
+// And the `comment-variables placeholders` command generates Comment Variables placeholders right next to where their values are defined.
+
+// For hover resolutions, you'll need the dedicated VS Code extension for Comment Variables.
+
+// Once you've grasped these concepts, simply rename this file, its JSON counterpart and its `.mjs` counterpart to `comments.config.js`, `comments.config.json` and `comments.config.mjs` respectively and you're good to go. (If you're using the extension, make sure to run VS Code's "Developer: Reload Window" command for the extension to operate based on your new `comments.config.js` file.)
